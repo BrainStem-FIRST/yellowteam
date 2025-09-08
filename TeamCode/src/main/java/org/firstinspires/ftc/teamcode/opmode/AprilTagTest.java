0eab2998402ec30ae9c5c5d8c9132ce87d697362
KeerthanaVisveish <keerthana.visveish@gmail.com>
2025-09-08 18:44:48 -0400

    april tag + dt changes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/AprilTagTest.java
@@ -0,0 +1,56 @@
+package org.firstinspires.ftc.teamcode.opmode;
+
+import android.util.Size;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
+@TeleOp (name = "April Tag", group = "Robot")
+public class AprilTagTest extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        AprilTagProcessor tagProcessor = new AprilTagProcessor.Builder()
+                .setDrawAxes(true)
+                .setDrawCubeProjection(true)
+                .setDrawTagID(true)
+                .setDrawTagOutline(true)
+                .build();
+
+        VisionPortal visionPortal = new VisionPortal.Builder()
+                .addProcessor(tagProcessor)
+                .setCamera(hardwareMap.get(WebcamName.class, "Webcam"))
+                .setCameraResolution(new Size(640, 480))
+                .build();
+
+        FtcDashboard dashboard = FtcDashboard.getInstance();
+        dashboard.startCameraStream(visionPortal, 0); //192.168.49.1:8080
+
+        waitForStart();
+
+        while (!isStopRequested() && opModeIsActive()) {
+            if (!tagProcessor.getDetections().isEmpty()){
+                AprilTagDetection tag = tagProcessor.getDetections().get(0);
+
+                telemetry.addData("x", tag.ftcPose.x);
+                telemetry.addData("y", tag.ftcPose.y);
+                telemetry.addData("z", tag.ftcPose.z);
+                telemetry.addData("roll", tag.ftcPose.roll);
+                telemetry.addData("pitch", tag.ftcPose.pitch);
+                telemetry.addData("yaw", tag.ftcPose.yaw);
+            }
+
+            telemetry.update();
+
+        }
+
+        visionPortal.close();
+
+    }
+}
