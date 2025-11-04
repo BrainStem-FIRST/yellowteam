6dbce88d9d5d4006a04f6b19b3524b7a196566f9
KeerthanaVisveish <keerthana.visveish@gmail.com>
2025-11-04 15:45:22 -0500

    merging auto changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/AutoPositions.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/AutoPositions.java
@@ -1,8 +1,13 @@
 package org.firstinspires.ftc.teamcode.utils;
 
+import androidx.annotation.NonNull;
+
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.acmerobotics.roadrunner.Action;
 import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.SequentialAction;
 import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.roadrunner.MecanumDrive;
 
@@ -11,13 +16,14 @@ public class AutoPositions {
 
     //Red positions
     public final Pose2d redFarShootingPosition = new Pose2d(50, 10, Math.toRadians(180));
-    public final Pose2d redCloseShootingPosition = new Pose2d(-35, 39.5, Math.toRadians(55));
-    public final Pose2d redFirstLine = new Pose2d(-13, 17.5, Math.toRadians(90));
-    public final Pose2d redSecondLine = new Pose2d(12, 17.5, Math.toRadians(90));
-    public final Pose2d redThirdLine = new Pose2d(27, 20, Math.toRadians(90));
+    public final Pose2d redCloseShootingPosition = new Pose2d(-40, 40, Math.toRadians(55));
+    public final Pose2d redFirstLine = new Pose2d(-13, 26, Math.toRadians(90));
+    public final Pose2d redSecondLine = new Pose2d(12, 26, Math.toRadians(90));
+    public final Pose2d redThirdLine = new Pose2d(32, 26, Math.toRadians(90));
     public final Pose2d redApproachHP = new Pose2d(35, 68, Math.toRadians(45));
+    public final Pose2d redEnd = new Pose2d(0, 45, Math.toRadians(90));
 
-   //blue positions
+    //blue positions
     public final Pose2d blueFarShootingPosition = new Pose2d(50, -10, Math.toRadians(180));
     public final Pose2d blueCloseShootingPosition = new Pose2d(-35, -39.5, Math.toRadians(-135));
     public final Pose2d blueFirstLine = new Pose2d(-13, -17.5, Math.toRadians(-90));
@@ -35,6 +41,12 @@ public class AutoPositions {
         return moveOffWall.build();
     }
 
+    public Action moveOffLine(boolean isClose) {
+        TrajectoryActionBuilder moveOffLine = drive.actionBuilder(redCloseShootingPosition)
+                .splineToLinearHeading(redEnd, Math.toRadians(90));
+        return moveOffLine.build();
+    }
+
     public Action driveCloseShootingPose(Pose2d startPose) {
         TrajectoryActionBuilder moveOffWall = drive.actionBuilder(startPose)
                 .lineToXLinearHeading(-35, Math.toRadians(55));
@@ -42,37 +54,38 @@ public class AutoPositions {
     }
 
     public Action firstLineShots(boolean isClose) {
-        TrajectoryActionBuilder firstLineShot = drive.actionBuilder(isClose ? redCloseShootingPosition : redFarShootingPosition)
-                .setTangent(Math.toRadians(0))
-                .splineToLinearHeading(redFirstLine, Math.toRadians(90))
-//                .waitSeconds(0.1)
+        Action firstLineShot = drive.actionBuilder(isClose ? redCloseShootingPosition : redFarShootingPosition)
+                .setTangent(Math.toRadians(-90))
+                .splineToLinearHeading(redFirstLine, Math.toRadians(90)).build();
+        TrajectoryActionBuilder secondPath = drive.actionBuilder(redFirstLine)
                 .lineToY(60)
-//                .waitSeconds(0.1)
-                .setReversed(true)
+                .setTangent(Math.toRadians(-135))
                 .splineToLinearHeading(isClose ? redCloseShootingPosition : redFarShootingPosition, isClose ? Math.toRadians(180) : Math.toRadians(0));
-        return firstLineShot.build();
+
+        Action maxTimeFirstPath = new TimedAction(firstLineShot, 5);
+
+        return new SequentialAction(
+                maxTimeFirstPath,
+                secondPath.build()
+        );
     }
 
     public Action secondLineShots(boolean isClose) {
         TrajectoryActionBuilder firstLineShot = drive.actionBuilder(isClose ? redCloseShootingPosition : redFarShootingPosition)
                 .setTangent(Math.toRadians(0))
                 .splineToLinearHeading(redSecondLine, Math.toRadians(90))
-//                .waitSeconds(0.1)
                 .lineToY(63)
-//                .waitSeconds(0.1)
-//                .setReversed(true)
-                .setTangent(Math.toRadians(-45))
+                .setTangent(Math.toRadians(-135))
                 .splineToLinearHeading(isClose ? redCloseShootingPosition : redFarShootingPosition, isClose ? Math.toRadians(135) : Math.toRadians(0));
         return firstLineShot.build();
     }
 
     public Action thirdLineShots(boolean isClose) {
         TrajectoryActionBuilder firstLineShot = drive.actionBuilder(redFarShootingPosition)
-                .splineTo(redThirdLine.position, redThirdLine.heading)
-                .waitSeconds(0.25)
+                .setTangent(Math.toRadians(0))
+                .splineToLinearHeading(redThirdLine, Math.toRadians(90))
                 .lineToY(63)
-                .waitSeconds(0.5)
-                .setReversed(true)
+                .setTangent(Math.toRadians(-135))
                 .splineToLinearHeading(isClose ? redCloseShootingPosition : redFarShootingPosition, isClose ? Math.toRadians(180) : Math.toRadians(0));
         return firstLineShot.build();
     }
