56d5eeb364e16bc85373dbadf960b07c7feeb18c
KeerthanaVisveish <keerthana.visveish@gmail.com>
2025-11-09 16:55:33 -0500

    crucible code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/PoseStorage.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/PoseStorage.java
@@ -3,6 +3,6 @@ package org.firstinspires.ftc.teamcode.utils;
 import com.acmerobotics.roadrunner.Pose2d;
 
 public class PoseStorage {
-    public static Pose2d currentPose = new Pose2d(0, 0, Math.toRadians(0));
+    public static Pose2d currentPose = new Pose2d(0, -48, Math.toRadians(0));
     public static int currentTurretEncoder = 0;
 }
